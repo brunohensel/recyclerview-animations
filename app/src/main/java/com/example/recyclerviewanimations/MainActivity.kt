e120de84f068e6a5ed93e311ee7588e80947552d
brunohensel <bruno.hensel@gmail.com>
2020-11-17 14:47:09 +0100

    added nav_graph and set up the ActionBarWithNavController

--- app/src/main/java/com/example/recyclerviewanimations/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewanimations/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.recyclerviewanimations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val navHostFragment: NavHostFragment =
+      supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+    val navController: NavController = navHostFragment.navController
+
+    val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+    setupActionBarWithNavController(navController, appBarConfiguration)
   }
+
+  override fun onSupportNavigateUp() =
+    Navigation.findNavController(this, R.id.nav_host_fragment)
+        .navigateUp()
 }
